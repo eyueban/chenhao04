82ebad484974ffde4176c9b5812a857d3da8d5df
eyueban <2016165447@qq.com>
2017-08-05 17:24:05 +0800

    dadadda

---
+++ src/test/java/TestHelloWorld.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by machenike on 2017/8/5.
+ */
+public class TestHelloWorld {
+    @Test
+    public void helloworld() {
+        System.out.println("hello world");
+    }
+}
